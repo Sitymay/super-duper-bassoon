5d555d38639bbfd2fac2ad9f1b2422e97b19504a
Sitymay <markelov25@gmail.com>
2025-05-22 17:24:41 +0300

    first commit

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -12,29 +15,95 @@ const (
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
 	// ваш код здесь
+	switch {
+	case size < 0:
+		return nil
+	case size == 0:
+		return []int{}
+	default:
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		numberSlice := make([]int, size)
+		for i := 0; i < size; i++ {
+			numberSlice[i] = r.Int()
+		}
+		return numberSlice
+	}
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
 	// ваш код здесь
+	if len(data) == 0 { // проверяем возможный пустой слайс
+		return 0
+	}
+
+	maxValue := data[0]          // берем начальный элемент за максимальное число
+	for _, value := range data { // пробегаемся по значениям слайса
+		if value > maxValue {
+			maxValue = value // обновляем максимальное число, если текущее значение больше
+		}
+	}
+	return maxValue // в ином случае возвращаем максимальное число
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
 	// ваш код здесь
+	if len(data) == 0 { // проверяем возможный пустой слайс
+		return 0
+	}
+
+	maxValueFromChunks := make([]int, CHUNKS) //создаем слайс храним в нем максимальное значение каждого чанка
+	var wg sync.WaitGroup
+
+	for i := 0; i < CHUNKS; i++ {
+
+		wg.Add(1)
+		chunkSize := len(data) / CHUNKS // делим общий слайс на 8 частей
+		go func(index int) {
+			defer wg.Done()
+			firstIndexChunk := index * chunkSize               // начальный индекс чанка
+			lastIndexChunk := firstIndexChunk + chunkSize      // конечный индекс чанка
+			chunkSlice := data[firstIndexChunk:lastIndexChunk] // срез начального и конечного значения чанка
+
+			// находим максимум в чанке
+			maxValueChunk := chunkSlice[0]
+			for _, value := range chunkSlice {
+				if value > maxValueChunk {
+					maxValueChunk = value
+				}
+
+			}
+			maxValueFromChunks[index] = maxValueChunk // сохраняем максимум в срез для максимумов всех чанков
+		}(i)
+	}
+	wg.Wait()
+	// ищем общий максимум из среза максимумов чанков
+	maxOfAllChunks := maxValueFromChunks[0]
+	for _, v := range maxValueFromChunks {
+		if v > maxOfAllChunks {
+			maxOfAllChunks = v
+		}
+	}
+	return maxOfAllChunks // возвращаем максимальное значение
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
 	// ваш код здесь
+	data := generateRandomElements(SIZE)
 
 	fmt.Println("Ищем максимальное значение в один поток")
 	// ваш код здесь
+	start := time.Now()
+	max := maximum(data)
+	elapsed := time.Since(start)
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed.Milliseconds())
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
-
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
 	// ваш код здесь
-
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	startChunks := time.Now()
+	maxChunks := maxChunks(data)
+	elapsedPar := time.Since(startChunks)
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", maxChunks, elapsedPar.Milliseconds())
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,59 @@
 package main
 
 // Пишите тесты в этом файле
+import (
+	"testing"
+)
+
+func TestGenerateRandomElementsNegativeSize(t *testing.T) {
+	result := generateRandomElements(-1)
+
+	if result != nil {
+		t.Error("Получен отрицательный размер слайса")
+	}
+}
+
+func TestGenerateRandomElementsZeroSize(t *testing.T) {
+	result := generateRandomElements(0)
+
+	if len(result) != 0 {
+		t.Error("Получена длина слайса не равная нулю")
+	}
+}
+
+func TestGenerateRandomElementsPositiveSize(t *testing.T) {
+	size := 500
+	result := generateRandomElements(size)
+
+	if len(result) != size {
+		t.Errorf("Ошибка! Ожидаемый размер %d, не совпадает с полученным %d", size, len(result))
+	}
+}
+
+func TestMaximumEmptySlice(t *testing.T) {
+	result := maximum([]int{}) // инициализируем пустой слайс
+	if result != 0 {
+		t.Error("Для пустого слайса ждали 0")
+	}
+}
+
+func TestMaximumSignleElement(t *testing.T) {
+	result := maximum([]int{1}) // инициализируем слайс с одним элементом
+	if result != 1 {
+		t.Error("Для слайса с одним элементом ждали 1")
+	}
+}
+
+func TestMaximumNegativeNumbers(t *testing.T) {
+	result := maximum([]int{-1, -2, -3}) // инициализируем слайс с отрицательными значениями
+	if result != -1 {
+		t.Error("Для слайса с отрицательными значениями ждали -1")
+	}
+}
+
+func TestMaximumPositiveNumbers(t *testing.T) {
+	result := maximum([]int{10, 3, 1}) // инициализируем слайс с положительными значениями
+	if result != 10 {
+		t.Error("Для слайса с положительными значениями ждали 10")
+	}
+}
